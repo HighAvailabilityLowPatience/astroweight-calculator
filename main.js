83c432bc0640261039f94607d902d9e0622009cc
Emmanuel Johnson <emmanueljohnso1998@gmail.com>
2026-02-20 16:49:09 -0500

    Add space-themed CSS and dynamic title initialization

--- main.js
+++ main.js
@@ -23,18 +23,6 @@ for (var i = 0; i < planets.length; i++) {
 
   planetSelect.appendChild(option);
 }
-        // We are going to solve this by breaking the problem into three parts. 
-        // Programmers like automating things, we'll populate the HTML drop down options using code, 
-        // instead of having to type out all the values. 
-        // Create a function that does the some math and gives us the new weight. 
-        // Then create a function that responds when the user clicks on the button. 
-
-        // 1. Populate the dropdown element with the data found in the planets array. 
-        // The value of each option should be the planet's name. 
-        // Use the following built-in methods: 
-        // `.forEach` `document.createElement` `document.getElementById` `.appendChild` 
-
-
   function calculateWeight(weight, planetName) {
   for (var i = 0; i < planets.length; i++) {
     var planet = planets[i];
@@ -46,21 +34,46 @@ for (var i = 0; i < planets.length; i++) {
 
   return "Invalid planet";
 }
-    function handleClickEvent(e) {
-        // 3. Declare a variable called userWeight and assign the value of the user's weight. 
+    function handleClickEvent(e) { 
 
-        var userWeight = document.getElementById("user-weight").value;
-
-        // 4. Delcare a variable called planetName and assign the name of the selected planet from the drop down. 
-var planetName = document.getElementById("planets").value;
-
-        // 5. Declare a variable called result and assign the value of the new calculated weight. 
-var result = calculateWeight(weight, planetName);
-        // 6. Write code to display the message shown in the screenshot. 
-
+        var userWeight = document.getElementById("user-weight").value; 
+var planetName = document.getElementById("planets").value; 
+var result = calculateWeight(userWeight,planetName);
     } 
+function displayMessage(){
+  var love = document.getElementById("title");
+  love.textContent = "I'm an Astro Weight Calculator"
+}
+function displayTitle(){
+ document.title = "Astro Calc"
+}
+const button = document.getElementById("calculate-button");
+button.addEventListener("click", handleClickEvent);
+displayMessage();
+displayTitle();
 
-        // 7. Set the #calculate-button element's onclick method to use the handleClickEvent function.
+
+
+
+ // We are going to solve this by breaking the problem into three parts. 
+        // Programmers like automating things, we'll populate the HTML drop down options using code, 
+        // instead of having to type out all the values. 
+        // Create a function that does the some math and gives us the new weight. 
+        // Then create a function that responds when the user clicks on the button. 
+
+        // 1. Populate the dropdown element with the data found in the planets array. 
+        // The value of each option should be the planet's name. 
+        // Use the following built-in methods: 
+        // `.forEach` `document.createElement` `document.getElementById` `.appendChild` DONE
+
+        // 3. Declare a variable called userWeight and assign the value of the user's weight. DONE
+
+        // 4. Delcare a variable called planetName and assign the name of the selected planet from the drop down. DONE 
+
+        // 5. Declare a variable called result and assign the value of the new calculated weight. DONE
+        // 6. Write code to display the message shown in the screenshot. DONE
+      // 7. Set the #calculate-button element's onclick method to use the handleClickEvent function. DONE
+
 
         // 8. Make it look nice by attaching  a style.css file to your index.html and writing some basic styling, 
         // feel free to add classes and id's to the HTML elements as you need, 
@@ -69,3 +82,8 @@ var result = calculateWeight(weight, planetName);
 
         // Bonus Challenges 
         // 8. Reverse the drop down order so that the sun is first.// Write your JavaScript code here!
+
+
+
+
+
